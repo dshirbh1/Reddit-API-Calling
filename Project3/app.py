0ec8190d35d4d7cfcc01a40776d22bf4844b114b
--disabled-login <dshirbh1@CS515-07.bu.binghamton.edu>
2023-12-09 20:01:23 -0500

    Dynamic analysis, analysis1 is just for reference

--- Project3/app.py
+++ Project3/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 # import base64
-import analysis2
+import analysis1
 # from io import BytesIO
 
 app = Flask(__name__)
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Get Matplotlib plot from your analysis module
-    analysis2.run_analysis2()
-    return render_template('index.html')
+    analysis1.run_analysis1()
+    return render_template('index2.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
